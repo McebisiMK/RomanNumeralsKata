a85e8482e6aaf23ff4fd775725a24e18d402ca13
mcebisimk <mcebisimkhohliwe@gmail.com>
2019-03-28 09:23:33 +0200

    Wrote tests for different scenarios

--- RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests.cs
@@ -10,15 +10,15 @@ namespace RomanNumeralsKata
         [TestCase(5, "V")]
         [TestCase(6, "VI")]
         [TestCase(9, "IX")]
-        public void GetRomanNumerals_GivenNumberFromOneToNine_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        public void GetRomanNumeral_GivenNumberFromUnitsCategory_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
         {
             //----------------Arrange--------------------------------
             var romanNumeralsService = CreateRomanNumeralsService();
 
-            //----------------Arrange--------------------------------
+            //----------------Act------------------------------------
             var actual = romanNumeralsService.GetRomanNumeral(number);
 
-            //----------------Arrange--------------------------------
+            //----------------Assert---------------------------------
             Assert.AreEqual(expected, actual);
 
         }
@@ -28,15 +28,15 @@ namespace RomanNumeralsKata
         [TestCase(50, "L")]
         [TestCase(60, "LX")]
         [TestCase(90, "XC")]
-        public void GetRomanNumerals_GivenNumberFromTenToNinety_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        public void GetRomanNumeral_GivenNumberFromTensCategory_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
         {
             //----------------Arrange--------------------------------
             var romanNumeralsService = CreateRomanNumeralsService();
 
-            //----------------Arrange--------------------------------
+            //----------------Act------------------------------------
             var actual = romanNumeralsService.GetRomanNumeral(number);
 
-            //----------------Arrange--------------------------------
+            //----------------Assert---------------------------------
             Assert.AreEqual(expected, actual);
 
         }
@@ -46,15 +46,15 @@ namespace RomanNumeralsKata
         [TestCase(500, "D")]
         [TestCase(600, "DC")]
         [TestCase(900, "CM")]
-        public void GetRomanNumerals_GivenNumberFromHundredToNineHundred_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        public void GetRomanNumeral_GivenNumberFromHundredsCategory_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
         {
             //----------------Arrange--------------------------------
             var romanNumeralsService = CreateRomanNumeralsService();
 
-            //----------------Arrange--------------------------------
+            //----------------Act------------------------------------
             var actual = romanNumeralsService.GetRomanNumeral(number);
 
-            //----------------Arrange--------------------------------
+            //----------------Assert---------------------------------
             Assert.AreEqual(expected, actual);
 
         }
@@ -62,35 +62,69 @@ namespace RomanNumeralsKata
         [TestCase(1000, "M")]
         [TestCase(2000, "MM")]
         [TestCase(3000, "MMM")]
-        public void GetRomanNumerals_GivenNumberFromOneThousandToThreeThousand_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        public void GetRomanNumeral_GivenNumberFromOneThousandsCategory_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
         {
             //----------------Arrange--------------------------------
             var romanNumeralsService = CreateRomanNumeralsService();
 
-            //----------------Arrange--------------------------------
+            //----------------Act------------------------------------
             var actual = romanNumeralsService.GetRomanNumeral(number);
 
-            //----------------Arrange--------------------------------
+            //----------------Assert----------------------------------
             Assert.AreEqual(expected, actual);
 
         }
 
-        [TestCase(10, "X")]
-        [TestCase(14, "XIV")]
-        [TestCase(15, "XV")]
+        [TestCase(11, "XI")]
+        [TestCase(54, "LIV")]
+        [TestCase(99, "XCIX")]
         [TestCase(16, "XVI")]
-        [TestCase(19, "XIX")]
-        public void GetRomanNumerals_GivenNumberFromTenToNineteen_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        [TestCase(49, "XLIX")]
+        public void GetRomanNumeral_GivenNumberThatIsACombinationOfTensAndUnits_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
         {
-            //----------------Arrange--------------------------------
+            //-----------------Arrange--------------------------------
             var romanNumeralsService = CreateRomanNumeralsService();
 
-            //----------------Arrange--------------------------------
+            //-----------------Act------------------------------------
             var actual = romanNumeralsService.GetRomanNumeral(number);
 
-            //----------------Arrange--------------------------------
+            //----------------Assert----------------------------------
             Assert.AreEqual(expected, actual);
 
+        }
+
+        [TestCase(111, "CXI")]
+        [TestCase(454, "CDLIV")]
+        [TestCase(555, "DLV")]
+        [TestCase(694, "DCXCIV")]
+        [TestCase(966, "CMLXVI")]
+        public void GetRomanNumeral_GivenNumberThatIsACombinationOfHundredsTensAndUnits_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        {
+            //-------------------Arrange-------------------------------
+            var romanNumeralsService = CreateRomanNumeralsService();
+
+            //-------------------Act-----------------------------------
+            var actual = romanNumeralsService.GetRomanNumeral(number);
+
+            //-------------------Assert--------------------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3111, "MMMCXI")]
+        [TestCase(2454, "MMCDLIV")]
+        [TestCase(3555, "MMMDLV")]
+        [TestCase(1694, "MDCXCIV")]
+        [TestCase(3966, "MMMCMLXVI")]
+        public void GetRomanNumeral_GivenNumberThatIsACombinationOfThousandsHundredsTensAndUnits_ShouldReturnCorrespondingRomanNumeral(int number, string expected)
+        {
+            //-------------------Arrange-------------------------------
+            var romanNumeralsService = CreateRomanNumeralsService();
+
+            //-------------------Act-----------------------------------
+            var actual = romanNumeralsService.GetRomanNumeral(number);
+
+            //-------------------Assert--------------------------------
+            Assert.AreEqual(expected, actual);
         }
 
         private static RomanNumeralsService CreateRomanNumeralsService()
